262ff9e0bdfb5eff133e55395ec37571ec1bbd82
José Manuel <josegonh28@hotmail.com>
2025-07-21 17:00:14 +0200

    v19

--- theme.js
+++ theme.js
@@ -1,14 +1,11 @@
 
 function setTheme(theme) {
   document.body.className = theme + " crt-lines";
-  document.querySelector('.pipboy-shell')?.classList.remove("verde", "naranja", "azul");
-  document.querySelector('.pipboy-shell')?.classList.add(theme);
   localStorage.setItem("pipboy-theme", theme);
 }
 window.onload = () => {
   const saved = localStorage.getItem("pipboy-theme");
   if (saved) {
     document.body.className = saved + " crt-lines";
-    document.querySelector('.pipboy-shell')?.classList.add(saved);
   }
 };
