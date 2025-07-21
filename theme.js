a085ddf123f11eee919dc52c9817f7d4c09ea5de
José Manuel <josegonh28@hotmail.com>
2025-07-21 18:13:33 +0200

    v29

--- theme.js
+++ theme.js
@@ -1,13 +1,14 @@
 function setTheme(theme) {
-  document.body.className = theme + " crt-lines";
+  document.body.className = theme;
   localStorage.setItem("pipboy-theme", theme);
 }
 
 window.onload = () => {
   const savedTheme = localStorage.getItem("pipboy-theme");
   if (savedTheme) {
-    document.body.className = savedTheme + " crt-lines";
+    document.body.className = savedTheme;
   }
 };
+
 
 
